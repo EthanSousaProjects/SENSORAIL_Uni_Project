06d97878fcd91e5a22f8a740dac6921277f8a00e
Ethan <ethans0708@gmail.com>
2024-10-30 12:48:57 +0000

    Created simplified functions, added in git ignore and started main file

---
+++ Main.py
@@ -0,0 +1,36 @@
+'''
+Main File to run on raspberry pi.
+'''
+# Packages to import
+from Redpitaya_Simplified_Functions import *
+import matplotlib.pyplot as plt
+
+# Constants
+Redpitaya_IP = "rp-f06501.local"
+
+# TESTING HERE REMOVE AFTER USE between these comments
+
+# Testing if board is online or not Fase is not online, true is online.
+print(Board_Online(Redpitaya_IP))
+
+if Board_Online(Redpitaya_IP) == True:
+
+    Frequency = 1000000
+    Waveform = "SINE"
+    Amplitude = 1
+    Channel_Number = 1
+    Decimation = 4 # Effectivly sample rate look at charts online.
+    # Start Continueos signal
+    Start_Continuous_Signal(Frequency,Waveform,Amplitude,Redpitaya_IP,Channel_Number)
+
+    data_Signal = Record_Signal(Redpitaya_IP,Channel_Number,Decimation)
+    
+    #Stop_Signal(Redpitaya_IP,Channel_Number)
+    Reset_Signal_All(Redpitaya_IP)
+
+    data_No_Signal = Record_Signal(Redpitaya_IP,Channel_Number,Decimation)
+    
+    plt.plot(data_Signal, 'bo')
+    plt.plot(data_No_Signal,'r+')
+    plt.show()
+# TESTING HERE REMOVE AFTER USE between these comments
---
+++ Redpitaya_Simplified_Functions.py
@@ -0,0 +1,98 @@
+'''
+This file will contain functions to interface with the redpitaya board.
+Mainly ones for generating and recording signals.
+This is simplified to work in the main program.
+
+It will communite using a direct ethernet link.
+'''
+
+# Packages to import
+import sys
+import time
+import redpitaya_scpi as scpi
+
+def Board_Online(IP):
+    '''
+    Function to check if board is online
+    IP - The IP address link to the redpitaya board use a "<hostname>.local" address.
+    '''
+    try:
+        rp_s = scpi.scpi(IP)
+        status = rp_s.tx_txt(':SYST:ERR?')
+        return True
+
+    except:
+        # Returned and error board likely off.
+        return False
+
+
+def Start_Continuous_Signal(Freq,Waveform,Amp,IP,Channel_Number):
+    '''
+    Function to start a signal on a specified output.
+    Freq - The frequency which the board should produce
+    Waveform - The signal to produce wave form. Options (SINE, SQUARE, TRIANGLE, SAWU,SAWD, PWM, ARBITRARY, DC, DC_NEG)
+    Amp - The amplitude of the signal to produce (between 1 and -1).
+    IP - The IP address link to the redpitaya board use a "<hostname>.local" address.
+    Channel_Number - The output channel number on the redpitaya board (either 1 or 2)
+    '''
+    rp_s = scpi.scpi(IP)
+
+    # Function for configuring Source (generating signal)
+    rp_s.sour_set(Channel_Number, Waveform, Amp, Freq, burst=False)
+
+    # Enable output
+    rp_s.tx_txt("OUTPUT" + str(Channel_Number) + ":STATE ON")
+    rp_s.tx_txt("SOUR" + str(Channel_Number) + ":TRig:INT")
+
+def Stop_Signal(IP,Channel_Number):
+    '''
+    Function to stop a specific channels signal.
+    IP - The IP address link to the redpitaya board use a "<hostname>.local" address.
+    Channel_Number - The output channel number on the redpitaya board (either 1 or 2)
+    '''
+    scpi.scpi(IP).tx_txt("SOUR" + str(Channel_Number) + ":STATE OFF")
+
+def Reset_Signal_All(IP):
+    '''
+    Function to resart all signal generation on a board
+    IP - The IP address link to the redpitaya board use a "<hostname>.local" address.
+    '''
+    scpi.scpi(IP).tx_txt('GEN:RST')
+
+
+def Record_Signal(IP,Channel_Number,Decimation):
+    '''
+    Function to record a signal off one of the input channels
+    IP - The IP address link to the redpitaya board use a "<hostname>.local" address.
+    Channel_Number - The input channel number on the redpitaya board (either 1 or 2)
+    Decimation - Basically setting the sampling rate. 
+        See https://redpitaya.readthedocs.io/en/latest/appsFeatures/examples/acquisition/acqRF-samp-and-dec.html 
+        Ranges include (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536)
+    '''
+
+    rp_s = scpi.scpi(IP)
+
+    rp_s.tx_txt('ACQ:RST')
+
+    # Function for configuring Acquisition
+    # Effectivlyu setting sampling rate
+    # See https://redpitaya.readthedocs.io/en/latest/appsFeatures/examples/acquisition/acqRF-samp-and-dec.html for more info
+    rp_s.acq_set(Decimation)
+
+    rp_s.tx_txt('ACQ:START')
+    rp_s.tx_txt('ACQ:TRig NOW')
+
+    while 1:
+        rp_s.tx_txt('ACQ:TRig:STAT?')
+        if rp_s.rx_txt() == 'TD':
+            break
+
+    ## ! OS 2.00 or higher only ! ##
+    while 1:
+        rp_s.tx_txt('ACQ:TRig:FILL?')
+        if rp_s.rx_txt() == '1':
+            break
+
+    Signal_Data = rp_s.acq_data(Channel_Number, convert= True)
+
+    return(Signal_Data)
