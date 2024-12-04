Fixed errors with all ae processing function calculation apply step

Sensitivty_Analysis_Script.py
@@ -78,30 +78,29 @@ def compute_all_ae_processing_algos(signal,sample_rate,lower_frequency,higher_fr
     # Set up to run functions one after another.
     spectrum = aepe.singal_to_Spectrum(signal)
 
-    return ([
-            aepe.band_energy(spectrum,sample_rate,lower_frequency,higher_frequency),
-            aepe.band_energy_ratio(spectrum,sample_rate,lower_frequency,higher_frequency),
-            aepe.clearance_factor(signal),
-            aepe.counts(signal,threshold),
-            aepe.crest_factor(signal),
-            aepe.energy(signal),
-            aepe.impulse_factor(signal),
-            aepe.k_factor(signal),
-            aepe.kurtosis(signal),
-            aepe.margin_factor(signal),
-            aepe.peak_amplitude(signal),
-            aepe.rms(signal),
-            aepe.shape_factor(signal),
-            aepe.singal_to_Spectrum(signal),
-            aepe.skewness(signal),
-            aepe.spectral_centroid(spectrum,sample_rate),
-            aepe.spectral_kurtosis(spectrum,sample_rate),
-            aepe.spectral_peak_frequency(spectrum,sample_rate),
-            aepe.spectral_rolloff(spectrum,sample_rate,roll_off),
-            aepe.spectral_skewness(spectrum,sample_rate),
-            aepe.spectral_variance(spectrum,sample_rate),
-            aepe.zero_crossing_rate(signal,sample_rate)
-            ])
+    return pd.Series({
+            "band_energy": aepe.band_energy(spectrum,sample_rate,lower_frequency,higher_frequency),
+            "band_energy_ratio": aepe.band_energy_ratio(spectrum,sample_rate,lower_frequency,higher_frequency),
+            "clearance_factor": aepe.clearance_factor(signal),
+            "counts": aepe.counts(signal,threshold),
+            "crest_factor": aepe.crest_factor(signal),
+            "energy": aepe.energy(signal),
+            "impulse_factor": aepe.impulse_factor(signal),
+            "k_factor": aepe.k_factor(signal),
+            "kurtosis": aepe.kurtosis(signal),
+            "margin_factor": aepe.margin_factor(signal),
+            "peak_amplitude": aepe.peak_amplitude(signal),
+            "rms": aepe.rms(signal),
+            "shape_factor": aepe.shape_factor(signal),
+            "skewness": aepe.skewness(signal),
+            "spectral_centroid": aepe.spectral_centroid(spectrum,sample_rate),
+            "spectral_kurtosis": aepe.spectral_kurtosis(spectrum,sample_rate),
+            "spectral_peak_frequency": aepe.spectral_peak_frequency(spectrum,sample_rate),
+            "spectral_rolloff": aepe.spectral_rolloff(spectrum,sample_rate,roll_off),
+            "spectral_skewness": aepe.spectral_skewness(spectrum,sample_rate),
+            "spectral_variance": aepe.spectral_variance(spectrum,sample_rate),
+            "zero_crossing_rate": aepe.zero_crossing_rate(signal,sample_rate)
+            })
 
 
     #TODO: Create this function to use apply on data frame to make code nicer for computing all properties.
@@ -128,6 +127,8 @@ for File in Files_In_Folder:
             lambda x: list(map(float, ast.literal_eval(x))) if pd.notnull(x) else [])
     Data_Frame['Signal'] = Data_Frame['Signal'].apply(np.array)
 
+    # Using the apply statement to calculate all ae properties for each data frame row
+    # Then adding it to the relevent columns for later processing.
     Data_Frame[[
         "band_energy",
         "band_energy_ratio",
@@ -146,33 +147,10 @@ for File in Files_In_Folder:
         "spectral_centroid",
         "spectral_kurtosis",
         "spectral_peak_frequency",
-        "spectral_rolloff"
+        "spectral_rolloff",
         "spectral_skewness",
         "spectral_variance",
-        "zero_crossing_rate"]] = 0.0
-
-    Data_Frame[[
-        "band_energy",
-        "band_energy_ratio",
-        "clearance_factor",
-        "counts",
-        "crest_factor",
-        "energy",
-        "impulse_factor",
-        "k_factor",
-        "kurtosis",
-        "margin_factor",
-        "peak_amplitude",
-        "rms",
-        "shape_factor",
-        "skewness",
-        "spectral_centroid",
-        "spectral_kurtosis",
-        "spectral_peak_frequency",
-        "spectral_rolloff"
-        "spectral_skewness",
-        "spectral_variance",
-        "zero_crossing_rate",
+        "zero_crossing_rate"
         ]] = Data_Frame.apply(
             lambda row: compute_all_ae_processing_algos(
                 row["Signal"],
@@ -183,12 +161,11 @@ for File in Files_In_Folder:
                 roll_off
                 ),axis=1)
 
-    print(Processed_Data_Frame)
-    #TODO: create this apply statement and get it all working as expected returning the processed data frame
+    print(Data_Frame)
+    print("Wait")
 
     #TODO: Create a function to make a string to run all of the relevent functions that a user defines in a list. Make it create the new columns in the data frame and run the funtions of the values.
     #TODO: create a function to execute the ae processing algos that we specify in a list. Make it a function.
 
 
-
-#TODO: Calculate the binomial probabilities for each property accross each file.
+    #TODO: Calculate the binomial probabilities for each property accross each file. Mean and standard deviation.